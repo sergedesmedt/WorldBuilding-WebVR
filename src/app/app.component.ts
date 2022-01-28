512d6a5878a582ab384f8ba78bf26101f0d8e6e5
sergedesmedt <sergedesmedt@yahoo.com>
2022-01-28 18:15:31 +0100

    basic cooperation with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, Input, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
+  @Input() pos = '0 1 -2';
   name = 'Angular ' + VERSION.major;
+
+  constructor() {}
+
+  ngOnInit() {}
 }
