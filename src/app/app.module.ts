dffba8067a0d7a7ccce27a946899553ce657086a
sergedesmedt <sergedesmedt@yahoo.com>
2022-02-02 17:49:54 +0100

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { MatSliderModule } from '@angular/material/slider';
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { toAFrame } from './toAFrame.pipe';
 import { BasicSceneComponent } from './BasicScene/basicscene.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, MatSliderModule],
   declarations: [AppComponent, HelloComponent, toAFrame, BasicSceneComponent],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
