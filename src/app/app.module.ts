cb4958ca8b19957361b64d5f9ad249ee228857f3
sergedesmedt <sergedesmedt@yahoo.com>
2022-02-04 05:50:59 +0100

    Removed angular material (i think)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { MatSliderModule } from '@angular/material/slider';
-
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { toAFrame } from './toAFrame.pipe';
 import { BasicSceneComponent } from './BasicScene/basicscene.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MatSliderModule],
+  imports: [BrowserModule, FormsModule],
   declarations: [AppComponent, HelloComponent, toAFrame, BasicSceneComponent],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
